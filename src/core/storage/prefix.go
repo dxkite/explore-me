440b4b979004cfd56ff9a459dbc5f1ab15b1193e
dxkite <dxkite@163.com>
2023-09-09 22:39:17 +0800

    feat: 调整文件名

--- src/core/storage/prefix.go
+++ src/core/storage/prefix.go
@@ -5,16 +5,16 @@ import (
 	"path"
 )
 
-type trimPrefixFS struct {
+type prefixFS struct {
 	src    http.FileSystem
 	prefix string
 }
 
-func (s *trimPrefixFS) Open(name string) (http.File, error) {
+func (s *prefixFS) Open(name string) (http.File, error) {
 	name = path.Join(s.prefix, name)
 	return s.src.Open(name)
 }
 
 func NewPrefix(prefix string, src http.FileSystem) http.FileSystem {
-	return &trimPrefixFS{src: src, prefix: prefix}
+	return &prefixFS{src: src, prefix: prefix}
 }
